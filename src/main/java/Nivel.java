551b823294502e73e950615283e53589aea5feb4
rodrigosg07 <rodrigosg1207@gmail.com>
2026-05-09 18:28:39 +0100

    class nivel

--- src/main/java/Nivel.java
+++ src/main/java/Nivel.java
@@ -1,3 +1,33 @@
 public class Nivel {
-    private String nada;
+    private int numero;
+    private int maxTentativas;
+    private int linhas;
+    private int colunas;
+
+    public Nivel(int numero, int maxTentativas, int linhas, int colunas) {
+        if (linhas <= 0 || colunas <= 0) throw new IllegalArgumentException("Dimensões de tabuleiro inválidas");
+        if (linhas * colunas % 2 != 0 ) throw new IllegalArgumentException("Dimensões de tabuleiro inválidas");
+        if (maxTentativas <= 0) throw new IllegalArgumentException("O maximo de tentativas deve ser positivo");
+        this.numero = numero;
+        this.maxTentativas = maxTentativas;
+        this.linhas = linhas;
+        this.colunas = colunas;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int getMaxTentativas() {
+        return maxTentativas;
+    }
+
+    public int getLinhas() {
+        return linhas;
+    }
+
+    public int getColunas() {
+        return colunas;
+    }
+
 }
